b9e7f560636b2da865121426fefb8b4528b783cb
charmyoung <charm0young@gmail.com>
2018-03-29 03:12:37 +0800

    test

--- modules/decision/decision_test.cpp
+++ modules/decision/decision_test.cpp
@@ -1,8 +1,12 @@
 
 #include <iostream>
+#include <random>
+#include <memory>
 
 #include "ros/ros.h"
+#include <tf/transform_listener.h>
 #include "geometry_msgs/PoseStamped.h"
+#include "modules/perception/map/costmap/costmap_interface.h"
 
 int main(int argc, char **argv){
   ros::init(argc,argv,"decision_test");
@@ -10,15 +14,40 @@ int main(int argc, char **argv){
   ros::NodeHandle rviz_nh("move_base_simple");
   ros::Publisher goal_pub = rviz_nh.advertise<geometry_msgs::PoseStamped>("goal",100);
   //first policy: random goal and judge valid, if valid then send goal, see if use global is wrong.
-  tf_ptr_ = std::make_shared<tf::TransformListener>(ros::Duration(10));
+  std::shared_ptr<tf::TransformListener> tf_ptr_ = std::make_shared<tf::TransformListener>(ros::Duration(10));
 
-  costmap_ptr_ = std::make_shared<rrts::perception::map::CostmapInterface>("decision_costmap",
+  std::shared_ptr<rrts::perception::map::CostmapInterface> costmap_ptr_ = std::make_shared<rrts::perception::map::CostmapInterface>("decision_costmap",
                                                                            *tf_ptr_,
                                                                            "modules/perception/map/costmap/config/costmap_parameter_config_for_global_plan.prototxt");
-  unsigned char * charmap_ = costmap_ptr_->GetCostMap()->GetCharMap();
-  ros::Rate loop_rate(10);
-  while(ros::ok()){
 
+
+  unsigned int gridmap_height_ =costmap_ptr_->GetCostMap()->GetSizeXCell();
+  unsigned int gridmap_width_ = costmap_ptr_->GetCostMap()->GetSizeYCell();
+  unsigned int size_ = gridmap_height_*gridmap_width_;
+  unsigned char * charmap_ = costmap_ptr_->GetCostMap()->GetCharMap();
+
+  unsigned int goal_index,goal_cell_x,goal_cell_y;
+  geometry_msgs::PoseStamped goal_;
+  goal_.pose.orientation.w = 1;
+  goal_.header.frame_id = "map";
+
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok()) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> uni_dis(0, size_ - 1);
+    while (true) {
+      unsigned int random_index = uni_dis(gen);
+      if (charmap_[random_index] < 240) {
+        goal_index=random_index;
+        break;
+      }
+    }
+    costmap_ptr_->GetCostMap()->Index2Cells(goal_index, goal_cell_x, goal_cell_y);
+    costmap_ptr_->GetCostMap()->Map2World(goal_cell_x, goal_cell_y, goal_.pose.position.x, goal_.pose.position.y);
+    goal_pub.publish(goal_);
     loop_rate.sleep();
   }
 }
